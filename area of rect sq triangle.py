a4d2cbb5c5cbaeb2cb774d0c1ae540a54129e8bd
Lubna-1 <152602744+Lubna-1@users.noreply.github.com>
2023-12-05 15:26:16 +0530

    Add files via upload

---
+++ area of rect sq triangle.py
@@ -0,0 +1,20 @@
+
+rect=lambda l,b:l*b
+l=float(input("enter the length:"))
+b=float(input("enter the breadth:"))
+print("area of rectangle is:",rect(l,b))
+
+
+
+
+square=lambda a:a*a
+a=float(input("enter the side:"))
+print("area of square is:",square(a))
+
+
+triangle=lambda b,h:0.5*b*h
+h=float(input("enter the height:"))
+b=float(input("enter the base:"))
+print("area of triangle is:",triangle(b,h))
+
+
